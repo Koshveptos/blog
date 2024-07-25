de2e1dbd09348d13979d17577034de000fe8a57d
koshveptos <vas.cazakov@yandex.ru>
2024-07-25 23:28:08 +0300

    create aqlalche user and blog

---
+++ backend/alembic/versions/03770e69eb58_create_user_and_blog_table_migrations.py
@@ -0,0 +1,24 @@
+"""create user and blog table migrations
+
+Revision ID: 03770e69eb58
+Revises: 2ee5608c6741
+Create Date: 2024-07-25 11:26:44.385386
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '03770e69eb58'
+down_revision = '2ee5608c6741'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
---
+++ backend/alembic/versions/2ee5608c6741_create_user_and_blog_table_migrations.py
@@ -0,0 +1,24 @@
+"""create user and blog table migrations
+
+Revision ID: 2ee5608c6741
+Revises: 
+Create Date: 2024-07-25 11:21:12.478530
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2ee5608c6741'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
